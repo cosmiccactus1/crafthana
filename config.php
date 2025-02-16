4004d68cbcfb6c35607c8d706a56441ec54a12d1
cosmiccactus1 <tarkun08@gmail.com>
2025-02-16 20:26:56 +0100

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-define('SMTP_HOST', 'smtp.zoho.eu');
-define('SMTP_PORT', '587');
-define('SMTP_USER', 'info@crafthana.xyz');
-define('SMTP_PASS', 'vasa_zoho_lozinka');
-define('SMTP_SECURE', 'tls');
-?>
