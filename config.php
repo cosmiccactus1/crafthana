768e6683c0a7efcd996b026428f1519b672fa9d0
cosmiccactus1 <tarkun08@gmail.com>
2025-02-19 20:25:09 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
-﻿<?php
+<?php
+// Povezivanje s varijablama okruženja u Vercelu
 define('SMTP_HOST', 'smtp.zoho.eu');
 define('SMTP_PORT', '587');
-define('SMTP_USER', 'info@crafthana.xyz');
-define('SMTP_PASS', 'vasa_zoho_lozinka');
+define('SMTP_USER', getenv('SMTP_USER'));  // Zoho korisničko ime
+define('SMTP_PASS', getenv('SMTP_PASS'));  // Zoho lozinka
 define('SMTP_SECURE', 'tls');
 ?>
