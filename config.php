f266fd3986deeaf354928a065191070c5e45d077
cosmiccactus1 <tarkun08@gmail.com>
2025-02-19 21:35:41 +0100

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Povezivanje s varijablama okruženja u Vercelu
-define('SMTP_HOST', 'smtp.zoho.eu');
-define('SMTP_PORT', '587');
-define('SMTP_USER', getenv('SMTP_USER'));  // Zoho korisničko ime
-define('SMTP_PASS', getenv('SMTP_PASS'));  // Zoho lozinka
-define('SMTP_SECURE', 'tls');
-?>
