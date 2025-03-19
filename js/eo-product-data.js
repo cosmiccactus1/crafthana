0cddff410b094bda569c1e733058a2c4ca6266b8
cosmiccactus1 <tarkun08@gmail.com>
2025-03-19 12:42:38 +0100

    Update eo-product-data.js

--- js/eo-product-data.js
+++ js/eo-product-data.js
@@ -161,10 +161,11 @@ const allEOProducts = {
     },
     
     // Special Blendovi
+  {
     "planinska-koliba": {
         id: "planinska-koliba",
         title: "Planinska Koliba",
-        tagline: "Zimska čarolija borova i toplog ognjišta",
+        tagline: "Prirodna harmonija borova i topline",
         volume: "10 ml",
         description: "Jedinstveni blend koji spaja svježinu planinskog zraka, miris borove šume i toplinu drvenog ognjišta. Ovaj kompleksni miris prenosi vas u ugodnu planinsku kolibu, okruženu snijegom i mirom prirode. Idealan za stvaranje zimske atmosfere i osjećaja topline i sigurnosti u vašem domu.",
         usage: "Idealno za električne difuzere. Dodajte 5-10 kapi u vodu u difuzeru za aromaterapiju cijelog prostora. Posebno preporučeno za zimske dane i večeri.",
@@ -187,12 +188,12 @@ const allEOProducts = {
         price: 11.99
     },
     
-    "jaffa-keks": {
-        id: "jaffa-keks",
-        title: "Jaffa Keks",
+    "special-narandza": {
+        id: "special-narandza",
+        title: "Special narandža",
         tagline: "Nostalgična kombinacija čokolade i naranče",
         volume: "10 ml",
-        description: "Sladak, nostalgičan miris koji savršeno oponaša omiljeni keks generacija. Bogata aroma čokolade isprepletena s osvježavajućim notama naranče stvara toplu, ugodnu atmosferu koja budi najljepše uspomene iz djetinjstva. Idealan za kuhinju, dnevni boravak ili bilo koji prostor gdje želite stvoriti ugodnu, slatku atmosferu.",
+        description: "Sladak, nostalgičan miris koji savršeno kombinira osvježavajuće note naranče s bogatom aromom čokolade. MMM jede mi se jaffa! Ovaj posebni blend stvara toplu, ugodnu atmosferu koja budi najljepše uspomene. Idealan za kuhinju, dnevni boravak ili bilo koji prostor gdje želite stvoriti ugodnu, slatku atmosferu.",
         usage: "Idealno za električne difuzere. Dodajte 5-10 kapi u vodu u difuzeru za aromaterapiju cijelog prostora. Posebno preporučeno za druženja i obiteljska okupljanja.",
         ingredients: "Pažljivo kreirana mješavina eteričnih ulja naranče, kakaa, vanilije i drugih prirodnih sastojaka u 100% čistom obliku.",
         category: "Essential Oil",
@@ -206,88 +207,36 @@ const allEOProducts = {
             "Maskira neugodne mirise i stvara ugodnu atmosferu"
         ],
         images: [
-            { src: "images/EOtest.jpg", alt: "Jaffa Keks essential oil" },
-            { src: "images/EOtest.jpg", alt: "Jaffa Keks detail 1" },
-            { src: "images/EOtest.jpg", alt: "Jaffa Keks detail 2" }
+            { src: "images/EOtest.jpg", alt: "Special narandža essential oil" },
+            { src: "images/EOtest.jpg", alt: "Special narandža detail 1" },
+            { src: "images/EOtest.jpg", alt: "Special narandža detail 2" }
         ],
         price: 11.99
     },
     
-    "zalazak-sunca": {
-        id: "zalazak-sunca",
-        title: "Zalazak Sunca",
-        tagline: "Topli cvjetni akordi s notom ambera",
+    "cejf": {
+        id: "cejf",
+        title: "Ćejf",
+        tagline: "Bosanska riječ koja opisuje najveći stepen zadovoljstva",
         volume: "10 ml",
-        description: "Bogata, topla aroma koja evocira mirne ljetne večeri i romantične zalaske sunca. Ovaj sofisticirani blend kombinira cvjetne note s toplim, dubokim akordima ambera i sandalovine, stvarajući uravnoteženu aromu koja opušta i smiruje duh. Idealan za večernje opuštanje i stvaranje romantične atmosfere.",
-        usage: "Idealno za električne difuzere. Dodajte 5-10 kapi u vodu u difuzeru za aromaterapiju cijelog prostora. Preporučeno za večernje sate i romantične trenutke.",
-        ingredients: "Kompozicija eteričnih ulja ruže, jasmina, ambera, sandalovine i drugih ekskluzivnih sastojaka u 100% čistom obliku.",
+        description: "Jedinstveni mirisni blend inspiriran tradicionalnim bosanskim zadovoljstvom - ćejfom. Ovaj posebni miris kombinira tople, opuštajuće note s blagom aromom koja stvara osjećaj potpunog zadovoljstva i mira. Idealan za one trenutke kada želite stvoriti atmosferu čistog užitka i opuštanja u svom domu.",
+        usage: "Idealno za električne difuzere. Dodajte 5-10 kapi u vodu u difuzeru za aromaterapiju cijelog prostora. Savršen za opuštanje nakon napornog dana.",
+        ingredients: "Pažljivo odabrana kombinacija umirujućih eteričnih ulja koja stvaraju harmoničnu aromu potpunog zadovoljstva, u 100% čistom obliku.",
         category: "Essential Oil",
         categoryType: "Special Blendovi",
         categoryLink: "essential-oil.html",
         benefits: [
-            "Stvara atmosferu opuštanja i mira",
-            "Idealan za romantične večeri",
-            "Smanjuje stres i anksioznost",
+            "Stvara atmosferu potpunog zadovoljstva i opuštanja",
+            "Umiruje um i tijelo nakon stresnog dana",
+            "Pomaže kod opuštanja i meditacije",
             "100% prirodna eterična ulja",
-            "Pomaže kod nesanice i nemira"
+            "Idealan za večernje opuštanje i bolji san"
         ],
         images: [
-            { src: "images/EOtest.jpg", alt: "Zalazak Sunca essential oil" },
-            { src: "images/EOtest.jpg", alt: "Zalazak Sunca detail 1" },
-            { src: "images/EOtest.jpg", alt: "Zalazak Sunca detail 2" }
-        ],
-        price: 11.99
-    },
-    
-    "kandirana-mandarina": {
-        id: "kandirana-mandarina",
-        title: "Kandirana Mandarina",
-        tagline: "Slatka citrusna fantazija",
-        volume: "10 ml",
-        description: "Sočna, svježa aroma mandarine obogaćena slatkim, kandiranim notama stvara jedinstven miris koji istovremeno osvježava i ugađa. Ovaj radostan blend savršen je za podizanje raspoloženja, posebno tijekom hladnijih mjeseci kada donosi dašak mediteranske svježine i topline u vaš dom.",
-        usage: "Idealno za električne difuzere. Dodajte 5-10 kapi u vodu u difuzeru za aromaterapiju cijelog prostora. Odlično za jutarnje buđenje i podizanje energije.",
-        ingredients: "Mandarina (Citrus reticulata), vanilija i drugi prirodni sastojci koji stvaraju efekat kandiranog voća, u 100% čistom obliku.",
-        category: "Essential Oil",
-        categoryType: "Special Blendovi",
-        categoryLink: "essential-oil.html",
-        benefits: [
-            "Podiže raspoloženje i energiju",
-            "Osvježava prostor i stvara ugodnu atmosferu",
-            "Smanjuje umor i mentalni napor",
-            "100% prirodna eterična ulja",
-            "Posebno popularan tijekom jesenskih i zimskih mjeseci"
-        ],
-        images: [
-            { src: "images/EOtest.jpg", alt: "Kandirana Mandarina essential oil" },
-            { src: "images/EOtest.jpg", alt: "Kandirana Mandarina detail 1" },
-            { src: "images/EOtest.jpg", alt: "Kandirana Mandarina detail 2" }
-        ],
-        price: 11.99
-    },
-    
-    "bijeli-musk": {
-        id: "bijeli-musk",
-        title: "Bijeli Musk",
-        tagline: "Čista elegancija i sofisticiranost",
-        volume: "10 ml",
-        description: "Raskošna, elegantna aroma inspirirana luksuznim parfemima, prilagođena za vaš dom. Ovaj sofisticirani blend stvara atmosferu čistoće, elegancije i luksuza. Bijeli musk ima suptilnu, puderastu i blago cvjetnu notu koja osvaja svojom bezvremenskom privlačnošću i univerzalnošću.",
-        usage: "Idealno za električne difuzere. Dodajte 5-10 kapi u vodu u difuzeru za aromaterapiju cijelog prostora. Savršen za dnevni boravak, spavaću sobu i prostore za primanje gostiju.",
-        ingredients: "Pažljivo balansirana formula prirodnih eteričnih ulja koja stvara profinjenu aromu bijelog muska, u 100% čistom obliku.",
-        category: "Essential Oil",
-        categoryType: "Special Blendovi",
-        categoryLink: "essential-oil.html",
-        benefits: [
-            "Stvara elegantnu i luksuznu atmosferu",
-            "Univerzalan miris koji odgovara svim prostorima",
-            "Dugotrajan i suptilan",
-            "100% prirodna eterična ulja",
-            "Pogodan za poslovne prostore i domove"
-        ],
-        images: [
-            { src: "images/EOtest.jpg", alt: "Bijeli Musk essential oil" },
-            { src: "images/EOtest.jpg", alt: "Bijeli Musk detail 1" },
-            { src: "images/EOtest.jpg", alt: "Bijeli Musk detail 2" }
+            { src: "images/EOtest.jpg", alt: "Ćejf essential oil" },
+            { src: "images/EOtest.jpg", alt: "Ćejf detail 1" },
+            { src: "images/EOtest.jpg", alt: "Ćejf detail 2" }
         ],
         price: 11.99
     }
-};
+}
